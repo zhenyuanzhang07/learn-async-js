e1dc85003677f73b88e487dbd4e38ce4cb42a4de
Zhenyuan Zhang <zhang.zhenyuan@northeastern.edu>
2024-02-29 15:01:14 -0500

    updated src/quiz/promise-sum-2darray.js and  src/quiz/promise-neg.js

--- src/quiz/promise-neg.js
+++ src/quiz/promise-neg.js
@@ -3,3 +3,27 @@ const array2D = [
     [4, 5, 6],
     [7, 8, -9]
 ];
+
+function checkRowForNegative(row, rowIndex) {
+    return new Promise((resolve) => {
+        if (row.some(number => number < 0)) {
+            resolve(`Row ${rowIndex} contains a negative number.`);
+        } else {
+            resolve(null);
+        }
+    });
+}
+
+function logNegativeRows(array2D) {
+    const promises = array2D.map((row, index) => checkRowForNegative(row, index));
+    Promise.all(promises)
+        .then(results => {
+            results.forEach(result => {
+                if (result) {
+                    console.log(result);
+                }
+            });
+        });
+}
+
+logNegativeRows(array2D);
--- src/quiz/promise-sum-2darray.js
+++ src/quiz/promise-sum-2darray.js
@@ -3,3 +3,23 @@ const array2D = [
     [4, 5, 6],
     [7, 8, 9]
 ];
+
+function sumRow(row) {
+    return new Promise((resolve) => {
+        let sum = 0;
+        for (let i = 0; i < row.length; i++) {
+            sum += row[i];
+        }
+        resolve(sum);
+    });
+}
+
+function sum2DArrayConcurrently(array2D) {
+    const promises = array2D.map(row => sumRow(row));
+    return Promise.all(promises)
+        .then(sums => sums.reduce((a, b) => a + b, 0));
+}
+
+sum2DArrayConcurrently(array2D)
+    .then(totalSum => console.log("Total Sum:", totalSum))
+    .catch(error => console.error("Error:", error));
